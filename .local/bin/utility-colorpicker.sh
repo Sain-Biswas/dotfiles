ffe2c2ab14583a48bd95e0dd6f17e5ac1e32fc07
Sain-Biswas <sain.biswas614@gmail.com>
2025-09-19 18:19:08 +0530

    feat: add screenshot utility

--- .local/bin/utility-colorpicker.sh
+++ .local/bin/utility-colorpicker.sh
@@ -4,31 +4,134 @@
 # ░█░░░█░█░█░░░█░█░█▀▄░░░█▀▀░░█░░█░░░█▀▄░█▀▀░█▀▄░░░█░█░░█░░░█░░█░░░░█░░░█░░░█░
 # ░▀▀▀░▀▀▀░▀▀▀░▀▀▀░▀░▀░░░▀░░░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀░▀░░░▀▀▀░░▀░░▀▀▀░▀▀▀░▀▀▀░░▀░░░▀░
 
-# Use rofi to display format options (HEX, RGB, HSL) and get the selected format
-format=$(echo -e "HEX\nRGB\nHSL" | rofi -dmenu -p "Select color format")
+colorpicker_rgb(){
+    hyprpicker | pastel format rgb | wl-copy
+    sleep 0.2
+    magick -size 64x64 xc:"$(wl-paste)" /tmp/color.png
+    notify-send "RGB color picked | Hyprpicker" "$(wl-paste)" -i /tmp/color.png -a "Hyprpicker"
+    rm /tmp/color.png
+    exit 0
+}
 
-# Display the color in the selected format
-case "$format" in
-    "HEX")
-        # Get HEX format using hyprpicker
-        color=$(hyprpicker -a --format=hex)
-        ;;
+
+colorpicker_hex(){
+    hyprpicker | pastel format hex | wl-copy
+    sleep 0.2
+    magick -size 64x64 xc:"$(wl-paste)" /tmp/color.png
+    notify-send "HEX color picked | Hyprpicker" "$(wl-paste)" -i /tmp/color.png -a "Hyprpicker"
+    rm /tmp/color.png
+    exit 0
+}
+
+
+colorpicker_hsv(){
+    hyprpicker | pastel format hsv | wl-copy
+    sleep 0.2
+    magick -size 64x64 xc:"$(wl-paste)" /tmp/color.png
+    notify-send "HSV color picked | Hyprpicker" "$(wl-paste)" -i /tmp/color.png -a "Hyprpicker"
+    rm /tmp/color.png
+    exit 0
+}
+
+colorpicker_hsl(){
+    hyprpicker | pastel format hsl | wl-copy
+    sleep 0.2
+    magick -size 64x64 xc:"$(wl-paste)" /tmp/color.png
+    notify-send "HSL color picked | Hyprpicker" "$(wl-paste)" -i /tmp/color.png -a "Hyprpicker"
+    rm /tmp/color.png
+    exit 0
+}
+
+colorpicker_lch(){
+    hyprpicker | pastel format lch | wl-copy
+    sleep 0.2
+    magick -size 64x64 xc:"$(wl-paste)" /tmp/color.png
+    notify-send "LCH color picked | Hyprpicker" "$(wl-paste)" -i /tmp/color.png -a "Hyprpicker"
+    rm /tmp/color.png
+    exit 0
+}
+
+colorpicker_lab(){
+    hyprpicker | pastel format lab | wl-copy
+    sleep 0.2
+    magick -size 64x64 xc:"$(wl-paste)" /tmp/color.png
+    notify-send "LAB color picked | Hyprpicker" "$(wl-paste)" -i /tmp/color.png -a "Hyprpicker"
+    rm /tmp/color.png
+    exit 0
+}
+
+colorpicker_oklab(){
+    hyprpicker | pastel format oklab | wl-copy
+    sleep 0.2
+    magick -size 64x64 xc:"$(wl-paste | pastel format hex)" /tmp/color.png
+    notify-send "okLAB color picked | Hyprpicker" "$(wl-paste)" -i /tmp/color.png -a "Hyprpicker"
+    rm /tmp/color.png
+    exit 0
+}
+
+colorpicker_luminance(){
+    hyprpicker | pastel format luminance | wl-copy
+    sleep 0.2
+    notify-send "Luminance color picked | Hyprpicker" "$(wl-paste)" -a "Hyprpicker"
+    rm /tmp/color.png
+    exit 0
+}
+
+colorpicker_brightness(){
+    hyprpicker | pastel format brightness | wl-copy
+    sleep 0.2
+    notify-send "Brightness color picked | Hyprpicker" "$(wl-paste)" -a "Hyprpicker"
+    rm /tmp/color.png
+    exit 0
+}
+
+colorpicker_name(){
+    hyprpicker | pastel format name | wl-copy
+    sleep 0.2
+    magick -size 64x64 xc:"$(wl-paste | pastel format hex)" /tmp/color.png
+    notify-send "Name color picked | Hyprpicker" "$(wl-paste)" -i /tmp/color.png -a "Hyprpicker"
+    rm /tmp/color.png
+    exit 0
+}
+
+colorpicker_command(){
+    echo -e "HEX\nHSL\nRGB\nHSV\nLCH\nLAB\nOKLAB\nLUMINANCE\nBRIGHTNESS\nNAME" | rofi -dmenu -theme ~/dotfiles/.config/rofi/colorpicker.rasi || exit 0
+}
+
+chosen="$(colorpicker_command)"
+
+case ${chosen} in
     "RGB")
-        # Get RGB format using hyprpicker
-        color=$(hyprpicker -a --format=rgb)
+        colorpicker_rgb
+        ;;
+    "HSV")
+        colorpicker_hsv
+        ;;
+    "HEX")
+        colorpicker_hex
         ;;
     "HSL")
-        # Get HSL format using hyprpicker
-        color=$(hyprpicker -a --format=hsl)
+        colorpicker_hsl
+        ;;
+    "LCH")
+        colorpicker_lch
+        ;;
+    "LAB")
+        colorpicker_lab
+        ;;
+    "OKLAB")
+        colorpicker_oklab
+        ;;
+    "LUMINANCE")
+        colorpicker_luminance
+        ;;
+    "BRIGHTNESS")
+        colorpicker_brightness
+        ;;
+    "NAME")
+        colorpicker_name
         ;;
     *)
-        echo "Invalid format selected, exiting..."
         exit 1
         ;;
 esac
-
-# Use ImageMagick to generate an image with the selected color
-convert -size 32x32 xc:$selected_color "/tmp/color_box.png"
-
-# Send a desktop notification with the selected color
-notify-send "Color Selected" "Color: $selected_color" -i "/tmp/color_box.png"
