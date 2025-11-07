7a15400021836ac90667bb5d98a409d52b5d04d9
Sain-Biswas <sain.biswas614@gmail.com>
2025-11-08 00:48:20 +0530

    chore: add new wallpapers

--- matugen.zsh
+++ matugen.zsh
@@ -6,13 +6,13 @@
 export FZF_DEFAULT_OPTS=" \
 --style full \
 --preview 'bat --color=always {}' --bind 'focus:transform-header:file --brief {}' \
---color=bg+:#3c3930,bg:#16130b,spinner:#ffb4ab,hl:#d2c6a1 \
---color=fg:#e9e2d4,header:#d2c6a1,info:#aad0b2,pointer:#ffb4ab \
---color=marker:#dcc66e,fg+:#e9e2d4,prompt:#aad0b2,hl+:#d2c6a1 \
---color=selected-bg:#dcc66e \
---color=border:#969080,label:#e9e2d4 \
+--color=bg+:#38393f,bg:#121318,spinner:#ffb4ab,hl:#c0c6dc \
+--color=fg:#e2e2e9,header:#c0c6dc,info:#e0bbdd,pointer:#ffb4ab \
+--color=marker:#b1c5ff,fg+:#e2e2e9,prompt:#e0bbdd,hl+:#c0c6dc \
+--color=selected-bg:#b1c5ff \
+--color=border:#8f9099,label:#e2e2e9 \
 --multi"
 
 
 # Aloxaf fzf - tab
-zstyle ':fzf-tab:*' fzf-flags --color=bg+:#3c3930,bg:#16130b,spinner:#ffb4ab,hl:#d2c6a1,fg:#e9e2d4,header:#d2c6a1,info:#aad0b2,pointer:#ffb4ab,marker:#dcc66e,fg+:#e9e2d4,prompt:#aad0b2,hl+:#d2c6a1,selected-bg:#dcc66e,border:#969080,label:#e9e2d4 --multi
+zstyle ':fzf-tab:*' fzf-flags --color=bg+:#38393f,bg:#121318,spinner:#ffb4ab,hl:#c0c6dc,fg:#e2e2e9,header:#c0c6dc,info:#e0bbdd,pointer:#ffb4ab,marker:#b1c5ff,fg+:#e2e2e9,prompt:#e0bbdd,hl+:#c0c6dc,selected-bg:#b1c5ff,border:#8f9099,label:#e2e2e9 --multi
